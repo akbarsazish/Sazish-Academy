48500d8e4e4acb1d41348ad573ce4dbbc7dad637
akbrsazish <sazishaliakbar@gmail.com>
2023-05-31 12:49:24 +0330

    Finish about page

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,7 @@
-// Get the current date and time
-var currentDate = new Date();
-
-var year = currentDate.getFullYear();      
-var month = currentDate.getMonth() + 1;    
-var day = currentDate.getDate();         
-
-document.getElementById("time").innerHTML = year + "-" + month + "-" + day +  " " + "(THU) ~ 16(FRI)"; 
 
 
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.getElementById('nav-menu');
-
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
@@ -18,7 +9,6 @@ hamburger.addEventListener('click', () => {
 });
 
 // speaker section data
-
 const speakerData = [
   {
     id: '1',
@@ -91,5 +81,13 @@ function generateSpeakerCard() {
 }
 
 
-
 generateSpeakerCard();
+
+// Get the current date and time
+var currentDate = new Date();
+
+var year = currentDate.getFullYear();      
+var month = currentDate.getMonth() + 1;    
+var day = currentDate.getDate();         
+
+document.getElementById("time").innerHTML = year + "-" + month + "-" + day +  " " + "(THU) ~ 16(FRI)"; 
